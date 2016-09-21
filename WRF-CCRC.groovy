a5fa48d675a7dbcf682b3ed78fd1dd13fa99b5a6
Scott Wales <scott.wales@unimelb.edu.au>
2016-09-21 15:39:20 +1000

    Change directory due to clash

--- WRF-CCRC.groovy
+++ WRF-CCRC.groovy
@@ -1,8 +1,8 @@
 node ('saw562.raijin') {
     stage 'extract'
-    sh 'rm -rf tests'
+    sh 'rm -rf jenkins-tests'
     git changelog: false, poll: false, url: 'https://bitbucket.org/ScottWales/unsw-ccrc-wrf', branch: 'ccrc-3.7.1'
-    sh 'git clone https://bitbucket.org/ScottWales/wrf-testing.git tests'
+    sh 'git clone https://bitbucket.org/ScottWales/wrf-testing.git jenkins-tests'
     
     env.WRF_ROOT = pwd()
     
@@ -24,7 +24,7 @@ EOF'''
         sh 'qsub -W depend=afterany:$(tail -n 1 configure.log) -W block=true -q express -l walltime=1:00 -- sleep 1'
     }
     
-    dir('tests'){
+    dir('jenkins-tests'){
         dir('jan00'){
             stage 'jan00'
             sh 'qsub -W block=true -v PROJECT,WRF_ROOT runtest.sh'
