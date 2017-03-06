53f64b4aa9407f089d199a01e796b767384edbb3
C. Carouge <c.carouge@unsw.edu.au>
2017-03-06 16:03:33 +1100

    Added umask to qsub
    So that outputs are readable by all.
    
    Signed-off-by: C. Carouge <c.carouge@unsw.edu.au>

--- WRF-Core.groovy
+++ WRF-Core.groovy
@@ -13,7 +13,7 @@ node ('saw562.raijin') {
 3
 1
 EOF'''
-        sh 'qsub -W depend=afterany:$(tail -n 1 configure.log) -W block=true -q express -l walltime=1:00 -- sleep 1'
+        sh 'qsub -W umask=0022 -W depend=afterany:$(tail -n 1 configure.log) -W block=true -q express -l walltime=1:00 -- sleep 1'
     }
     
     stage 'compile_WPS'
@@ -22,24 +22,24 @@ EOF'''
 3
 1
 EOF'''
-        sh 'qsub -W depend=afterany:$(tail -n 1 configure.log) -W block=true -q express -l walltime=1:00 -- sleep 1'
+        sh 'qsub -W umask=0022 -W depend=afterany:$(tail -n 1 configure.log) -W block=true -q express -l walltime=1:00 -- sleep 1'
     }
     
     dir('tests'){
         dir('jan00'){
             stage 'jan00'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT runtest.sh'
+            sh 'qsub -W umask=0022 -W block=true -v PROJECT,WRF_ROOT runtest.sh'
             sh 'module load cdo; cdo diff wrfout_d01_2000-01-24_12\\:00\\:00 /projects/WRF/data/KGO/3.7.1/jan00/wrfout_d01_2000-01-24_12\\:00\\:00'
         }
         dir('jan00-nesting'){
             stage 'jan00-nesting'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT runtest.sh'
+            sh 'qsub -W umask=0022 -W block=true -v PROJECT,WRF_ROOT runtest.sh'
             sh 'module load cdo; cdo diff wrfout_d01_2000-01-24_12\\:00\\:00 /projects/WRF/data/KGO/3.7.1/jan00-nesting/wrfout_d01_2000-01-24_12\\:00\\:00'
             sh 'module load cdo; cdo diff wrfout_d02_2000-01-24_12\\:00\\:00 /projects/WRF/data/KGO/3.7.1/jan00-nesting/wrfout_d02_2000-01-24_12\\:00\\:00'
         }
         dir('jan00-diagnostics'){
             stage 'jan00-diagnostics'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT runtest.sh'
+            sh 'qsub -W umask=0022 -W block=true -v PROJECT,WRF_ROOT runtest.sh'
             sh 'module load cdo; for file in wrfxtrm_d*_2000-01-24_12\\:00\\:00; do cdo diff $file /projects/WRF/data/KGO/3.7.1/jan00-diagnostics/$file; done'
         }
     }
