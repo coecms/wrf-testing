6743ecfd500bdbade0c5dd13dda63c2560b07c5e
C. Carouge <c.carouge@unsw.edu.au>
2017-05-09 13:44:19 +1000

    Copy modifications from WRF-Core.groovy + comments

--- WRF-CCRC.groovy
+++ WRF-CCRC.groovy
@@ -1,64 +1,65 @@
 node ('saw562.raijin') {
     stage 'extract'
+    // Get the tests. Clone the wrf-testing repository again in jenkins-tests/
+    // Then checkout the branch for the tested version as indicated in params.VERSION
     sh 'rm -rf jenkins-tests'
-    git changelog: false, poll: false, url: 'https://bitbucket.org/ccarouge/unsw-ccrc-wrf-perso', branch: 'new_version'
+    git changelog: false, poll: false, url: 'https://bitbucket.org/ccarouge/unsw-ccrc-wrf-perso', branch: "V${params.VERSION}"
     sh 'git clone https://bitbucket.org/ccarouge/wrf-testing.git jenkins-tests'
+    dir('jenkins-tests') {
+       sh "git branch --track ${params.VERSION} origin/${params.VERSION}"
+       sh "git checkout ${params.VERSION}"  
+    }
 
     currentBuild.displayName += ' ' + params.VERSION
     env.WRF_ROOT = pwd()
 
     stage 'clean_WRF'
+    // Clean previous WRFV3 compilation if wanting to start from scratch (optional)
     dir('WRFV3') {
        if (params.CLEAN_WRF == true) {
-          sh 'echo starting clean'
           sh './clean -a'
        }
        sh 'qsub -W umask=0022 -W block=true -q express -l walltime=1:00 -- sleep 1'   
     }
 
     stage 'clean_WPS'
+    // Clean previous WPS compilation if wanting to start from scratch (optional)
     dir('WPS') {
        if (params.CLEAN_WPS == true) {
-          sh 'echo starting clean'
           sh './clean -a'
        }
        sh 'qsub -W umask=0022 -W block=true -q express -l walltime=1:00 -- sleep 1'
     }
 
     stage 'compile_WRF'
+    // Compile WRFV3
     dir('WRFV3') {
-        sh '''./run_compile > configure.log << EOF
-3
-1
-EOF'''
-        sh 'qsub -W umask=0022 -W depend=afterany:$(tail -n 1 configure.log) -W block=true -q express -l walltime=1:00 -- sleep 1'
+        sh 'qsub -W umask=0022 -W block=true -q express ./run_compile'
     }
 
     stage 'compile_WPS'
+    // Compile WPS
     dir('WPS') {
-        sh '''./run_compile > configure.log << EOF
-3
-1
-EOF'''
-        sh 'qsub -W umask=0022 -W depend=afterany:$(tail -n 1 configure.log) -W block=true -q express -l walltime=1:00 -- sleep 1'
+        sh 'qsub -W umask=0022 -W block=true -q express ./run_compile'
     }
 
     dir('jenkins-tests'){
+        // Start run tests.
         dir('jan00'){
             stage 'jan00'
             sh 'qsub -W umask=0022 -W block=true -v PROJECT,WRF_ROOT runtest.sh'
-            sh 'module load cdo; cdo diffn wrfout_d01_2000-01-24_12\\:00\\:00 /projects/WRF/data/KGO/3.7.1/jan00/wrfout_d01_2000-01-24_12\\:00\\:00'
+            sh "module load cdo; cdo diffn wrfout_d01_2000-01-24_12\\:00\\:00 /projects/WRF/data/KGO/${params.VERSION}/jan00/wrfout_d01_2000-01-24_12\\:00\\:00"
         }
         dir('jan00-nesting'){
             stage 'jan00-nesting'
             sh 'qsub -W umask=0022 -W block=true -v PROJECT,WRF_ROOT runtest.sh'
-            sh 'module load cdo; cdo diffn wrfout_d01_2000-01-24_12\\:00\\:00 /projects/WRF/data/KGO/3.7.1/jan00-nesting/wrfout_d01_2000-01-24_12\\:00\\:00'
-            sh 'module load cdo; cdo diffn wrfout_d02_2000-01-24_12\\:00\\:00 /projects/WRF/data/KGO/3.7.1/jan00-nesting/wrfout_d02_2000-01-24_12\\:00\\:00'
+            sh "module load cdo; cdo diffn wrfout_d01_2000-01-24_12\\:00\\:00 /projects/WRF/data/KGO/${params.VERSION}/jan00-nesting/wrfout_d01_2000-01-24_12\\:00\\:00"
+            sh "module load cdo; cdo diffn wrfout_d02_2000-01-24_12\\:00\\:00 /projects/WRF/data/KGO/${params.VERSION}/jan00-nesting/wrfout_d02_2000-01-24_12\\:00\\:00"
         }
         dir('jan00-diagnostics'){
             stage 'jan00-diagnostics'
             sh 'qsub -W umask=0022 -W block=true -v PROJECT,WRF_ROOT runtest.sh'
-            sh 'module load cdo; for file in wrfxtrm_d*_2000-01-24_12\\:00\\:00; do cdo diffn $file /projects/WRF/data/KGO/3.7.1/jan00-diagnostics/$file; done'
+            sh "module load cdo; for file in wrfxtrm_d*_2000-01-24_12\\:00\\:00; do cdo diffn \$file /projects/WRF/data/KGO/${params.VERSION}/jan00-diagnostics/\$file; done"
         }
     }
 
