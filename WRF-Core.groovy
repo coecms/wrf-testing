8e38118a8d2e81348dfb1dda22b1a9ca93491a41
C. Carouge <c.carouge@unsw.edu.au>
2021-05-13 14:18:36 +1000

    Doesn't make sense to have 1 test branch per version. So hard-wire the test branch number in the file

--- WRF-Core.groovy
+++ WRF-Core.groovy
@@ -7,8 +7,8 @@ node ('saw562.raijin') {
     // git changelog: false, poll: false, url: "/projects/WRF/WRFV_${params.VERSION}"
     sh 'git clone https://github.com/coecms/wrf-testing.git tests'
     dir('tests') {
-       sh "git branch --track ${params.VERSION} origin/${params.VERSION}"
-       sh "git checkout ${params.VERSION}"  
+       sh "git branch --track 3.9.1.1 origin/3.9.1.1"
+       sh "git checkout 3.9.1.1"  
     }
 
     currentBuild.displayName += ' ' + params.VERSION
