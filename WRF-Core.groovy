734fd9d48e136dd7b97439cf19a3d5997563523f
Claire Carouge <ccc561@gadi-login-01.gadi.nci.org.au>
2021-05-13 14:15:42 +1000

    Optional stages

--- WRF-Core.groovy
+++ WRF-Core.groovy
@@ -52,58 +52,68 @@ node ('ccc561.gadi') {
     dir('tests'){
     // Start run tests.
         dir('jan00'){
-            stage 'jan00'
-            sh 'cp ../../WRFV3/run/run_real ../../WRFV3/run/run_mpi ../../WPS/run_WPS.sh .'
-            sh 'cp ../../WPS/namelist.wps.jan00 namelist.wps'
-            sh 'cp ../../WRFV3/test/em_real/namelist.input.jan00 namelist.input'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_WPS.sh'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_real'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
-            sh "module load cdo; cdo diffn wrfout_d01_2000-01-24_12\\:00\\:00 /g/data/sx70/data/KGO/${params.VERSION}/jan00/wrfout_d01_2000-01-24_12\\:00\\:00"
-        }
+            if (params.JAN00 == true) {
+            	stage 'jan00'
+            	sh 'cp ../../WRFV3/run/run_real ../../WRFV3/run/run_mpi ../../WPS/run_WPS.sh .'
+            	sh 'cp ../../WPS/namelist.wps.jan00 namelist.wps'
+            	sh 'cp ../../WRFV3/test/em_real/namelist.input.jan00 namelist.input'
+            	sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_WPS.sh'
+            	sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_real'
+            	sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
+            	sh "module load cdo; cdo diffn wrfout_d01_2000-01-24_12\\:00\\:00 /g/data/sx70/data/KGO/${params.VERSION}/jan00/wrfout_d01_2000-01-24_12\\:00\\:00"
+            }
+	    }
         dir('jan00-nesting'){
-            stage 'jan00-nesting'
-            sh 'cp ../../WRFV3/run/run_real ../../WRFV3/run/run_mpi ../../WPS/run_WPS.sh .'
-            sh 'cp ../../WPS/namelist.wps.jan00-nesting namelist.wps'
-            sh 'cp ../../WRFV3/test/em_real/namelist.input.jan00-nesting namelist.input'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_WPS.sh'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_real'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
-            sh "module load cdo; cdo diffn wrfout_d01_2000-01-24_12\\:00\\:00 /g/data/sx70/data/KGO/${params.VERSION}/jan00-nesting/wrfout_d01_2000-01-24_12\\:00\\:00"
-            sh "module load cdo; cdo diffn wrfout_d02_2000-01-24_12\\:00\\:00 /g/data/sx70/data/KGO/${params.VERSION}/jan00-nesting/wrfout_d02_2000-01-24_12\\:00\\:00"
+            if (params.NESTING == true) {
+                stage 'jan00-nesting'
+                sh 'cp ../../WRFV3/run/run_real ../../WRFV3/run/run_mpi ../../WPS/run_WPS.sh .'
+                sh 'cp ../../WPS/namelist.wps.jan00-nesting namelist.wps'
+                sh 'cp ../../WRFV3/test/em_real/namelist.input.jan00-nesting namelist.input'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_WPS.sh'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_real'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
+                sh "module load cdo; cdo diffn wrfout_d01_2000-01-24_12\\:00\\:00 /g/data/sx70/data/KGO/${params.VERSION}/jan00-nesting/wrfout_d01_2000-01-24_12\\:00\\:00"
+                sh "module load cdo; cdo diffn wrfout_d02_2000-01-24_12\\:00\\:00 /g/data/sx70/data/KGO/${params.VERSION}/jan00-nesting/wrfout_d02_2000-01-24_12\\:00\\:00"
+            }
         }
         dir('jan00-diagnostics'){
-            stage 'jan00-diagnostics'
-            sh 'cp ../../WRFV3/run/run_real ../../WRFV3/run/run_mpi ../../WPS/run_WPS.sh .'
-            sh 'cp ../../WPS/namelist.wps.jan00-nesting namelist.wps'
-            sh 'cp ../../WRFV3/test/em_real/namelist.input.jan00-diagnostics namelist.input'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_WPS.sh'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_real'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
-            sh "module load cdo; for file in wrfxtrm_d*_2000-01-24_12\\:00\\:00; do cdo diffn \$file /g/data/sx70/data/KGO/${params.VERSION}/jan00-diagnostics/\$file; done"
+            if (params.DIAG == true) {
+                stage 'jan00-diagnostics'
+                sh 'cp ../../WRFV3/run/run_real ../../WRFV3/run/run_mpi ../../WPS/run_WPS.sh .'
+                sh 'cp ../../WPS/namelist.wps.jan00-nesting namelist.wps'
+                sh 'cp ../../WRFV3/test/em_real/namelist.input.jan00-diagnostics namelist.input'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_WPS.sh'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_real'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
+                sh "module load cdo; for file in wrfxtrm_d*_2000-01-24_12\\:00\\:00; do cdo diffn \$file /g/data/sx70/data/KGO/${params.VERSION}/jan00-diagnostics/\$file; done"
+            }
         }
         dir('jan00-quilting'){
-            stage 'jan00-quilting'
-            sh 'cp ../../WRFV3/run/run_real ../../WRFV3/run/run_mpi ../../WPS/run_WPS.sh .'
-            sh 'cp ../../WPS/namelist.wps.jan00 namelist.wps'
-            sh 'cp ../../WRFV3/test/em_real/namelist.input.jan00-quilting namelist.input'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_WPS.sh'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_real'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
-            sh "module load cdo; cdo diffn wrfout_d01_2000-01-24_12\\:00\\:00 /g/data/sx70/data/KGO/${params.VERSION}/jan00/wrfout_d01_2000-01-24_12\\:00\\:00"
+            if (params.QUILTING == true) {
+                stage 'jan00-quilting'
+                sh 'cp ../../WRFV3/run/run_real ../../WRFV3/run/run_mpi ../../WPS/run_WPS.sh .'
+                sh 'cp ../../WPS/namelist.wps.jan00 namelist.wps'
+                sh 'cp ../../WRFV3/test/em_real/namelist.input.jan00-quilting namelist.input'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_WPS.sh'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_real'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
+                sh "module load cdo; cdo diffn wrfout_d01_2000-01-24_12\\:00\\:00 /g/data/sx70/data/KGO/${params.VERSION}/jan00/wrfout_d01_2000-01-24_12\\:00\\:00"
+            }
         }
         dir('jan00-restart'){
-            stage 'jan00-restart'
-            sh 'cp ../../WRFV3/run/run_real ../../WRFV3/run/run_mpi ../../WPS/run_WPS.sh .'
-            sh 'cp ../../WPS/namelist.wps.jan00 namelist.wps'
-            sh 'cp ../../WRFV3/test/em_real/namelist.input.jan00-restart* .'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_WPS.sh'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_real'
-            sh 'cp namelist.input.jan00-restart1 namelist.input'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
-            sh 'cp namelist.input.jan00-restart2 namelist.input'
-            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
-            sh './compare_output.sh'
+            if (params.RESTART == true) {
+                stage 'jan00-restart'
+                sh 'cp ../../WRFV3/run/run_real ../../WRFV3/run/run_mpi ../../WPS/run_WPS.sh .'
+                sh 'cp ../../WPS/namelist.wps.jan00 namelist.wps'
+                sh 'cp ../../WRFV3/test/em_real/namelist.input.jan00-restart* .'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_WPS.sh'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_real'
+                sh 'cp namelist.input.jan00-restart1 namelist.input'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
+                sh 'cp namelist.input.jan00-restart2 namelist.input'
+                sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
+                sh './compare_output.sh'
+            }
         }
 	dir('UPP'){
             dir('postprd'){
