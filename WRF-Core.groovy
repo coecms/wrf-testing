ffde12a9d0386fa744cf7115959dd5e1d21d7722
Claire Carouge <ccc561@gadi-login-04.gadi.nci.org.au>
2021-05-13 14:19:15 +1000

    Add restart test

--- WRF-Core.groovy
+++ WRF-Core.groovy
@@ -13,6 +13,7 @@ node ('ccc561.gadi') {
 
     currentBuild.displayName += ' ' + params.VERSION
     env.WRF_ROOT = pwd()
+    env.KGO_ROOT = '/g/data/sx70/data/KGO/${params.VERSION}'
 
     stage 'clean_WRF' 
     // Clean previous WRFV3 compilation if wanting to start from scratch (optional)
@@ -90,6 +91,19 @@ node ('ccc561.gadi') {
             sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_real'
             sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
             sh "module load cdo; cdo diffn wrfout_d01_2000-01-24_12\\:00\\:00 /g/data/sx70/data/KGO/${params.VERSION}/jan00/wrfout_d01_2000-01-24_12\\:00\\:00"
+        }
+        dir('jan00-restart'){
+            stage 'jan00-restart'
+            sh 'cp ../../WRFV3/run/run_real ../../WRFV3/run/run_mpi ../../WPS/run_WPS.sh .'
+            sh 'cp ../../WPS/namelist.wps.jan00 namelist.wps'
+            sh 'cp ../../WRFV3/test/em_real/namelist.input.jan00-restart* .'
+            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_WPS.sh'
+            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_real'
+            sh 'cp namelist.input.jan00-restart1 namelist.input'
+            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
+            sh 'cp namelist.input.jan00-restart2 namelist.input'
+            sh 'qsub -W block=true -v PROJECT,WRF_ROOT run_mpi'
+            sh './compare_output.sh'
         }
 	dir('UPP'){
             dir('postprd'){
