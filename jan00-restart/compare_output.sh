cdce35f6ee6953ebe158fff7459f50286d7f9b29
Claire Carouge <ccc561@gadi-login-04.gadi.nci.org.au>
2021-05-13 14:15:42 +1000

    Typo in restart test

--- jan00-restart/compare_output.sh
+++ jan00-restart/compare_output.sh
@@ -4,7 +4,7 @@ module load nco
 module load cdo
 
 # Extract last time step
-nco -d Time,-1,-1 wrfout_d01_2000-01-24_18:00:00 restart_out.nc
-nco -d Time,-1,-1 $path/jan00/wrfout_d01_2000-01-24_12:00:00 jan00_out.nc
+ncks -d Time,-1,-1 wrfout_d01_2000-01-24_21:00:00 restart_out.nc
+ncks -d Time,-1,-1 /g/data/sx70/data/KGO/4.1.2/jan00/wrfout_d01_2000-01-24_12:00:00 jan00_out.nc
 
 cdo diffn restart_out.nc jan00_out.nc
