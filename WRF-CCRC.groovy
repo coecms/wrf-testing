a7ab8c291652e3b0f6438db5bfb25b1a9b5d853e
C. Carouge <c.carouge@unsw.edu.au>
2021-05-13 14:18:36 +1000

    Remove unnecessary sleep jobs

--- WRF-CCRC.groovy
+++ WRF-CCRC.groovy
@@ -19,7 +19,6 @@ node ('saw562.raijin') {
        if (params.CLEAN_WRF == true) {
           sh './clean -a'
        }
-       sh 'qsub -W umask=0022 -W block=true -q express -l walltime=1:00 -- sleep 1'   
     }
 
     stage 'clean_WPS'
@@ -28,7 +27,6 @@ node ('saw562.raijin') {
        if (params.CLEAN_WPS == true) {
           sh './clean -a'
        }
-       sh 'qsub -W umask=0022 -W block=true -q express -l walltime=1:00 -- sleep 1'
     }
 
     stage 'compile_WRF'
