c8b3a6ff22ee6e685c9bff4aacf1d182de38dd53
C. Carouge <c.carouge@unsw.edu.au>
2022-03-24 15:42:34 +1100

    Update name of wrfout for restart

--- oct16-restart/compare_output.sh
+++ oct16-restart/compare_output.sh
@@ -4,7 +4,7 @@ module load nco
 module load cdo
 
 # Extract last time step
-ncks -d Time,-1,-1 wrfout_d01_2016-10-07_03:00:00 restart_out.nc
+ncks -d Time,-1,-1 wrfout_d01_2016-10-06_06:00:00 restart_out.nc
 ncks -d Time,-1,-1 /g/data/sx70/data/KGO/4.3/oct16/wrfout_d01_2016-10-06_00:00:00 oct16_out.nc
 
 cdo diffn restart_out.nc oct16_out.nc
