ab575002160d2a61acd64f6df2966fae4bfc73b2
C. Carouge <c.carouge@unsw.edu.au>
2022-05-06 12:27:44 +1000

    Update version in restart test

--- oct16-restart/compare_output.sh
+++ oct16-restart/compare_output.sh
@@ -5,6 +5,6 @@ module load cdo
 
 # Extract last time step
 ncks -d Time,-1,-1 wrfout_d01_2016-10-06_06:00:00 restart_out.nc
-ncks -d Time,-1,-1 /g/data/sx70/data/KGO/4.3/oct16/wrfout_d01_2016-10-06_00:00:00 oct16_out.nc
+ncks -d Time,-1,-1 /g/data/sx70/data/KGO/4.4/oct16/wrfout_d01_2016-10-06_00:00:00 oct16_out.nc
 
 cdo diffn restart_out.nc oct16_out.nc
